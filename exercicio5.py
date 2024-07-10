ee303b7e15a234095e50dee602eeaec7f86feb8e
Vinicius Irapuã <viniciusirapua.souza2@gmail.com>
2024-07-10 13:49:42 -0300

    fim

--- exercicio5.py
+++ exercicio5.py
@@ -1,5 +1,9 @@
-var_inteiro = int(input("digite o valor inteiro:"))
-var_decimal = float(input("digite o valor decimal:"))
-var_palavra = str(input("digite a palavra:"))
+# Solicita ao usuário um número inteiro
+var_inteiro = int(input("Digite o valor inteiro: "))
+# Solicita ao usuário um número decimal
+var_decimal = float(input("Digite o valor decimal usando '.': "))
+# Solicita ao usuário uma palavra 
+var_palavra = str(input("Digite uma palavra: "))
 
+# exibe as informações digitadas formatadas
 print(f"o numero inteiro é {var_inteiro}, o decimal é {var_decimal}, e a palavra é {var_palavra}.")
---
+++ exercicio6.py
@@ -0,0 +1,9 @@
+# Recebe do usuário um número e aemazena
+var_numero1 = float(input("Digite um número: "))
+# Recebe do usuário um número e armazena
+var_numero2 = float(input("Digite um número: "))
+# Recebe do usuário um número e armazena
+var_numero3 = float(input("Digite um número: "))
+var_media = (var_numero1 + var_numero2 + var_numero3) /3
+ 
+print(f"a média é {var_media:.2f}.")
