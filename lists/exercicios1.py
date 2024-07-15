a072247a1780d50e319b071d493846bffb83d725
Vinicius Irapuã <viniciusirapua.souza2@gmail.com>
2024-07-15 16:52:14 -0300

    Create exercicios1.py

---
+++ lists/exercicios1.py
@@ -0,0 +1,7 @@
+minha_lista  = ['banana', 'maçã', 'uva', 'pera']
+print(minha_lista[1])
+minha_lista.append('laranja')
+print(minha_lista)
+print("Quantidade de frutas na lista:", len(minha_lista))
+minha_lista.remove('pera')
+print(minha_lista)
